bb2ff7943943bf95cd83cd2e5d7844dd36a78a31
houvven <houvven@gmail.com>
2025-11-20 19:12:43 +0800

    refactor(lib): restructure plugin initialization and add log rotation

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,46 +4,48 @@ use tauri_plugin_decorum::WebviewWindowExt;
 mod commands;
 mod window;
 
-#[cfg(debug_assertions)]
-fn prevent_default() -> tauri::plugin::TauriPlugin<tauri::Wry> {
-    tauri_plugin_prevent_default::debug()
+fn log_plugin() -> tauri::plugin::TauriPlugin<tauri::Wry> {
+    use tauri_plugin_log::{Target, TargetKind};
+    tauri_plugin_log::Builder::new()
+        .targets([
+            Target::new(TargetKind::Stdout),
+            Target::new(TargetKind::LogDir { file_name: None }),
+        ])
+        .level(if cfg!(debug_assertions) {
+            LevelFilter::Debug
+        } else {
+            LevelFilter::Info
+        })
+        .max_file_size(1024 * 1024 * 10)
+        .rotation_strategy(tauri_plugin_log::RotationStrategy::KeepAll)
+        .build()
 }
 
-#[cfg(not(debug_assertions))]
-fn prevent_default() -> tauri::plugin::TauriPlugin<tauri::Wry> {
-    tauri_plugin_prevent_default::init()
+fn prevent_default_plugin() -> tauri::plugin::TauriPlugin<tauri::Wry> {
+    if cfg!(debug_assertions) {
+        tauri_plugin_prevent_default::debug()
+    } else {
+        tauri_plugin_prevent_default::init()
+    }
 }
 
 pub fn run() {
     use log::info;
     use tauri::Manager;
-    use tauri_plugin_log::{Target, TargetKind};
 
     tauri::Builder::default()
-        .plugin(
-            tauri_plugin_log::Builder::new()
-                .targets([
-                    Target::new(TargetKind::Stdout),
-                    Target::new(TargetKind::LogDir { file_name: None }),
-                ])
-                .level(if cfg!(debug_assertions) {
-                    LevelFilter::Debug
-                } else {
-                    LevelFilter::Info
-                })
-                .build(),
-        )
-        .plugin(prevent_default())
-        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .plugin(log_plugin())
         .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             if let Some(win) = app.get_webview_window(window::MAIN_WINDOW_LABEL) {
                 win.set_focus().unwrap();
             }
         }))
+        .plugin(prevent_default_plugin())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_decorum::init())
-        .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_opener::init())
         .manage(commands::ProcessingState::default())
         .invoke_handler(tauri::generate_handler![
             commands::upscale_image,
@@ -55,7 +57,9 @@ pub fn run() {
 
             main_window.create_overlay_titlebar().unwrap();
             #[cfg(target_os = "macos")]
-            main_window.set_traffic_lights_inset(16.0, 20.0).unwrap();
+            {
+                main_window.set_traffic_lights_inset(16.0, 20.0).unwrap();
+            }
 
             Ok(())
         })
